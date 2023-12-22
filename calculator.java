decbe232cebfcad9cea55eb96ec0d373d83451cf
Astha-31 <116273972+Astha-31@users.noreply.github.com>
2023-12-22 13:53:42 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Simple Calculator");
+            System.out.println("1. Addition (+)");
+            System.out.println("2. Subtraction (-)");
+            System.out.println("3. Multiplication (*)");
+            System.out.println("4. Division (/)");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice (1-5): ");
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Exiting the calculator. Goodbye!");
+                break;
+            }
+
+            System.out.print("Enter the first number: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Enter the second number: ");
+            double num2 = scanner.nextDouble();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Result: " + (num1 + num2));
+                    break;
+                case 2:
+                    System.out.println("Result: " + (num1 - num2));
+                    break;
+                case 3:
+                    System.out.println("Result: " + (num1 * num2));
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        System.out.println("Result: " + (num1 / num2));
+                    } else {
+                        System.out.println("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a number between 1 and 5.");
+            }
+
+            System.out.println(); // Add a newline for better readability
+        }
+
+        scanner.close();
+    }
+}
